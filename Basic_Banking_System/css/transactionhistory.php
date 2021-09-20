ad9de2bbf82058b7d7be6cbab7e01433bbd2b396
rututawase <91047651+rututawase@users.noreply.github.com>
2021-09-20 12:00:44 +0530

    Add files via upload

---
+++ Basic_Banking_System/css/transactionhistory.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transaction History</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/table.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+     <style>
+       .bg-img {
+  /* The image used */
+  background-image: url("img/a4.jpg");
+
+  width: 100%;
+  height: 100vh;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+   </style>
+</head>
+
+<body >
+ <div align="center" class="bg-img">
+<?php
+  include 'nav.php';
+?>
+
+	<div class="container">
+        <h2 class="text-center pt-4" style="color : black;">Transaction History</h2>
+        
+       <br>
+       <div class="table-responsive-sm">
+    <table class="table table-hover table-striped table-condensed table-bordered" style="color : black; background-color:rgba(255,255,255,0.3);">
+        <thead style="color : black;">
+            <tr>
+                <th class="text-center">S.No.</th>
+                <th class="text-center">Sender</th>
+                <th class="text-center">Receiver</th>
+                <th class="text-center">Amount</th>
+                <th class="text-center">Date & Time</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+
+            include 'config.php';
+
+            $sql ="select * from transaction";
+
+            $query =mysqli_query($conn, $sql);
+
+            while($rows = mysqli_fetch_assoc($query))
+            {
+        ?>
+
+            <tr style="color : black;">
+            <td class="py-2"><?php echo $rows['sno']; ?></td>
+            <td class="py-2"><?php echo $rows['sender']; ?></td>
+            <td class="py-2"><?php echo $rows['receiver']; ?></td>
+            <td class="py-2"><?php echo $rows['balance']; ?> </td>
+            <td class="py-2"><?php echo $rows['datetime']; ?> </td>
+                
+        <?php
+            }
+
+        ?>
+        </tbody>
+    </table>
+
+    </div>
+</div>
+<?php include('footer.php'); ?>
+</body>
+</html>
